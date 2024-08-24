854e9404bcc257db717549d4f12413fa6caaac66
alok7139 <alokgarg7139@gmail.com>
2024-08-24 22:55:09 +0530

    commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,35 +8,35 @@ function Hero() {
   
 
   return (
-    <div className='flex flex-col md:flex-row justify-between items-center mt-20 md:mt-44 px-4 md:px-10 pt-16 md:pt-0'>
-  <div className='flex flex-col md:w-1/2'>
-    <h1 className='font-bold text-3xl md:text-4xl text-yellow-700'>
+    <div className='flex flex-col lg:flex-row justify-between items-center mt-20 lg:mt-44 px-4 lg:px-10 pt-16 lg:pt-0'>
+  <div className='flex flex-col lg:w-1/2'>
+    <h1 className='font-bold text-3xl lg:text-4xl text-yellow-700'>
       "Connecting Neighbors, <span className='ml-4 md:ml-0'>Building Community"</span>
     </h1>
     
-    {/* <p className=' mt-5 text-base md:text-lg text-gray-600 font-sans'>
-      <div className='flex flex-col text-lg md:text-xl gap-1 font-serif'>
+    {/* <p className=' mt-5 text-base lg:text-lg text-gray-600 font-sans'>
+      <div className='flex flex-col text-lg lg:text-xl gap-1 font-serif'>
         <span>Join a <span className='text-violet-800 font-bold'>LocalLink</span> platform where neighbors come together to share, help, and grow.</span>
         <span>Whether it's funding, Handyman, or supporting small businesses,</span>
         <span>our platform brings people closer.</span>
       </div>
     </p> */}
-    <p className=' mt-5 text-base md:text-2xl text-gray-600 font-serif'>
+    <p className=' mt-5 text-base lg:text-2xl text-gray-600 font-serif'>
   Join a <span className='text-violet-800 font-bold'>LocalLink</span> platform where neighbors come together to share, help, and grow. Whether it's Local Events, Handyman, or supporting small businesses, our platform brings people closer.
 </p>
 
     
     <div className="mt-8 ">
-      <button className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-600">
+      <button className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-blue-600">
         Join the Community
       </button>
     </div>
   </div>
   
-  <div className='mt-10 md:mt-0 md:ml-10'>
+  <div className='mt-10 lg:mt-0 lg:ml-10'>
     <img 
       src='https://www.cypherlearning.com/hubfs/blog/entrepreneurs/posts/2022/10/INDIE-blog-01-Top-4-online-community-platforms-for-knowledge-entrepreneurs.png' 
-      className='rounded-3xl sm:w-full md:w-[800px] h-auto'
+      className='rounded-3xl sm:w-full lg:w-[800px] h-auto'
     />
   </div>
 </div>
--- client/src/components/Servicespreview.jsx
+++ client/src/components/Servicespreview.jsx
@@ -6,22 +6,53 @@ import ImageSlider from './Imageslider'
 function Servicespreview() {
   return (
     <>
-     <div className='mt-10'>
+     <div className='mt-12'>
         <div className='flex flex-col justify-center items-center m-5 p-5'>
         <div >
-          <div className='flex flex-row justify-between items-center'>
-             <h1 className='font-serif text-5xl font-bold flex flex-col'>
+          <div className='flex justify-center items-center'>
+             <h1 className='font-serif md:text-5xl  text-3xl font-bold flex flex-col'>
              <span className=''>Get the most out of</span>
               <span>your neighborhood</span>
                <span>with LocalLink</span>
              </h1>
 
-             <div>
-              <p></p>
-             </div>
+             
           </div>
           
         </div>
+
+         <div className='flex flex-col justify-center items-center mt-10'>
+             <div className='flex flex-row items-center justify-center gap-5'>
+             <h1 className='  font-bold text-3xl'>Local Events :</h1>
+             <p className='mt-1'>Users can create and participate in discussions about local events happening in their neighborhood, such as block parties, community meetings, or festivals.</p>
+             </div>
+             <div className='flex flex-row items-center justify-center gap-5 mt-8'>
+             <h1 className='  font-bold text-3xl'>Gardening Services :</h1>
+             <p className='mt-1'> A dedicated section where neighbors can offer or inquire about gardening services, share tips, and discuss community gardening projects.</p>
+             </div>
+             <div className='flex flex-row items-center justify-center gap-5 mt-8'>
+             <h1 className='  font-bold text-3xl'>Lost Pets :</h1>
+             <p className='mt-1'>A place to post and find information about lost or found pets in the neighborhood, helping reunite pets with their owners.</p>
+             </div>
+             <div className='flex flex-row items-center justify-center gap-5 mt-8'>
+             <h1 className='  font-bold text-3xl'>Tutoring :</h1>
+             <p className='mt-1'>A space where neighbors can offer or seek tutoring services, whether it's for school subjects, languages, music lessons, or other educational needs.</p>
+             </div>
+             <div className='flex flex-row items-center justify-center gap-5 mt-8'>
+             <h1 className='  font-bold text-3xl'>Handyman :</h1>
+             <p className='mt-1'>Users can ask for recommendations or offer handyman services, such as home repairs, electrical work, or plumbing, within the neighborhood.</p>
+             </div>
+             <div className='flex flex-row items-center justify-center gap-5 mt-8'>
+             <h1 className='  font-bold text-3xl'>Sales :</h1>
+             <p className='mt-1'>Neighbors can list items they are selling or announce garage sales, making it easy to buy and sell within the community.</p>
+             </div>
+         </div>
+
+         <div className='mt-10'> 
+          <button className='rounded-lg bg-purple-800 m-4 text-white p-4 text-center'>
+            SIgn up
+          </button>
+         </div>
         </div>
      </div>
     </> 
