42264b4e74b82833e6867c9f48a64c9776e9bd80
alok7139 <alokgarg7139@gmail.com>
2024-08-27 00:54:36 +0530

    commit

--- server/Controller/messagecontroller.js
+++ server/Controller/messagecontroller.js
@@ -1,3 +1,21 @@
 import { Message } from "../models/contactmodel.js";
+import ErrorHandler from "../middlewares/error.js";
+import { catchasyncerror } from "../middlewares/catchasyncerror.js";
+
+export const message = catchasyncerror(async(req,res,next) => {
+    const {name , email , phone , message} = req.body;
+
+    if(!name || !email || !phone || !message){
+        return new ErrorHandler("Fill all the details" , 400);
+    }
+
+    await Message.create({name ,email , phone , message});
+
+    return res.status(201).json({
+        success:true,
+        message:"Thanks for your query"
+    })
+})
+
 
 
