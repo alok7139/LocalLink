42acf0c354cabc7e5853fc7711dec402e7df7df1
alok7139 <alokgarg7139@gmail.com>
2024-09-06 18:30:38 +0530

    commit

--- server/routes/localeventsroute.js
+++ server/routes/localeventsroute.js
@@ -5,9 +5,9 @@ import express from 'express'
 const router = express.Router();
 
 router.post('/register/events' , isauthorized , registerevents);
-router.get('/getall/events' ,isauthorized, getalluserevents)
+router.get('/getall/events' , getalluserevents)
 router.put('/update/event/:id' , isauthorized , updatevents);
 router.delete('/delete/event/:id' , isauthorized , deleteevents);
-router.get('/user/allevents' , isauthorized , allevents)
+router.get('/user/allevents'  , allevents)
 
 export default router;
