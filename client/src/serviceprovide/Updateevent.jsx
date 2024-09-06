dc6040304b083e1e81443bc747b99a8777aa6570
alok7139 <alokgarg7139@gmail.com>
2024-09-06 23:24:22 +0530

    commit

--- client/src/serviceprovide/Updateevent.jsx
+++ client/src/serviceprovide/Updateevent.jsx
@@ -2,18 +2,51 @@ import React, { useContext, useState } from 'react'
 import { Context } from '../main';
 import { Navigate, useParams } from 'react-router-dom';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 function Updateevent() {
-    const { isauthenticated, setisauthenticated } = useContext(Context);
+    const {user, isauthenticated, setisauthenticated } = useContext(Context);
     const {id} = useParams();
-    const [localeventname, setlocaleventname] = useState('')
-    
+    const [localeventname, setlocaleventname] = useState(user && user.localeventname)
+    const [startdate, setstartdate] = useState(user && user.startdate)
+    const [enddate, setenddate] = useState(user && user.enddate)
+    const [owner, setowner] = useState(user && user.owner)
+    const [address, setaddress] = useState(user && user.address)
+    const [phone, setphone] = useState(user && user.phone)
+    const [city, setcity] = useState(user && user.city)
+    const [localeventsvg, setlocaleventsvg] = useState(user && user.localeventsvg && user.localeventsvg.url)
+    const [localeventsvgpreview, setlocaleventsvgpreview] = useState(user && user.localeventsvg && user.localeventsvg.url)
 
+    const handlesvg = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            setlocaleventsvg(file)
+            setlocaleventsvgpreview(reader.result)
+        }
+    }
+
+
+
+
+ 
     const handleupdate = async(id) => {
+        const formdata = new FormData();
+        formdata.append("localeventname" , localeventname)
+        formdata.append("startdate" , startdate)
+        formdata.append("enddate" , enddate);
+        formdata.append("owner" , owner)
+        formdata.append("address" , address)
+        formdata.append("phone" , phone);
+        formdata.append("city" , city)
+        formdata.append("localeventsvg" , localeventsvg)
         try {
-            const res = await axios.put(`http://localhost:3000/api/v1/update/event/${id}` , {} , {withCredentials:true})
+            const res = await axios.put(`http://localhost:3000/api/v1/update/event/${id}` , formdata ,{withCredentials:true , headers:{"Content-Type" : "multipart/form-data"}})
+            toast.success(res.data.message);
+
         } catch (error) {
-            
+            toast.error(error.response.data.message);
         }
     }
 
