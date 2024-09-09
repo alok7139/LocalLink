ed7ef7e3007d3fba1c188a9e63ee5ec039843dd9
alok7139 <alokgarg7139@gmail.com>
2024-09-09 19:43:10 +0530

    commit

--- client/src/serviceprovide/Createevents.jsx
+++ client/src/serviceprovide/Createevents.jsx
@@ -18,7 +18,36 @@ function Createevents() {
   const [city, setcity] = useState('');
   const [localeventsvg, setlocaleventsvg] = useState('');
   const [localeventsvgpreview, setlocaleventsvgpreview] = useState('');
-  const [first, setfirst] = useState(second)
+  const [error,seterror] = useState({localeventname: '' , startdate: '' , enddate: '' , owner :'' , address: '' , phone:'' , city:'' , localeventsvg:''});
+
+  const validateform = () => {
+    const newerrors = {};
+    if(!localeventname){
+      newerrors.localeventname = 'Event Name is required';
+    }
+    if(!startdate){
+      newerrors.startdate = 'Start Date is required';
+    }
+    if(!enddate){
+      newerrors.enddate = 'End Date is required';
+    }
+    if(!owner){
+      newerrors.owner = 'Event Holder Name is required';
+    }
+    if(!address){
+      newerrors.address = 'Address is required';
+    }
+    if(!phone){
+      newerrors.phone = 'Phone Number is required';
+    }
+    if(!city){
+      newerrors.city = 'City is required';
+    }
+    if(!localeventsvg){
+      newerrors.localeventsvg = 'Event Preview is required';
+    }
+    return newerrors;
+  }
 
   const handlesvg = (e) => {
     const file = e.target.files[0];
@@ -34,6 +63,13 @@ function Createevents() {
 
   const handleregister = async(e) => {
       e.preventDefault();
+      
+      const validateForm = validateform();
+      if(Object.keys(validateForm).length> 0){
+        seterror(validateForm);
+        return ;
+      }
+
       const formdata = new FormData();
       formdata.append('localeventname', localeventname);
     formdata.append('startdate', startdate);
@@ -74,63 +110,90 @@ function Createevents() {
             : 'https://images.vexels.com/media/users/3/131734/isolated/preview/05d86a9b63d1930d6298b27081ddc345-photo-preview-frame-icon.png'
         }
         alt={localeventname}
-        className='w-full h-full object-cover rounded-sm'
+        className='w-full h-auto max-w-xl object-cover rounded-sm'
       />
-      <input type='file' className='mt-4 w-full p-2 border rounded-lg' onChange={handlesvg} />
+      <input type='file' className={`mt-4 ${error.localeventsvg ? 'border-red-500' : 'border-gray-300'} w-full p-2 border rounded-lg`} onChange={handlesvg} />
+      {error.localeventsvg && <p className='text-red-500 mt-2 text-sm'>{error.localeventsvg}</p>}
     </div>
 
     <div className='w-full md:w-2/3 lg:w-2/3 space-y-4'>
-      <input
-        type='text'
-        className='w-full p-2 border rounded-lg'
-        value={localeventname}
-        placeholder='Enter the Event Name'
-        onChange={(e) => setlocaleventname(e.target.value)}
-      />
-      <div className='flex flex-col sm:flex-row gap-4'>
+      <div className='flex flex-col w-full'>
         <input
-          type='date'
-          className='w-full sm:w-1/2 p-2 border rounded-lg'
-          value={startdate}
-          placeholder='Start Date'
-          onChange={(e) => setstartdate(e.target.value)}
+          type='text'
+          className={`w-full p-2 ${error.localeventname ? 'border-red-500' : 'border-gray-300'} border rounded-lg`}
+          value={localeventname}
+          placeholder='Enter the Event Name'
+          onChange={(e) => setlocaleventname(e.target.value)}
         />
-        <input
-          type='date'
-          className='w-full sm:w-1/2 p-2 border rounded-lg'
-          value={enddate}
-          placeholder='End Date'
-          onChange={(e) => setenddate(e.target.value)}
-        />
+        {error.localeventname && <p className='text-red-500 mt-2 text-sm'>{error.localeventname}</p>}
       </div>
-      <input
-        type='text'
-        className='w-full p-2 border rounded-lg'
-        value={owner}
-        placeholder='Enter the Event Holder Name'
-        onChange={(e) => setowner(e.target.value)}
-      />
-      <input
-        type='text'
-        className='w-full p-2 border rounded-lg'
-        value={address}
-        placeholder='Enter the Address'
-        onChange={(e) => setaddress(e.target.value)}
-      />
-      <input
-        type='text'
-        className='w-full p-2 border rounded-lg'
-        value={phone}
-        placeholder='Enter the Phone Number'
-        onChange={(e) => setphone(e.target.value)}
-      />
-      <input
-        type='text'
-        className='w-full p-2 border rounded-lg'
-        value={city}
-        placeholder='Enter the City'
-        onChange={(e) => setcity(e.target.value)}
-      />
+
+      <div className='flex flex-col w-full sm:flex-row gap-4'>
+        <div className='flex flex-col w-full'>
+          <input
+            type='date'
+            className={`w-full sm:w-full md:w-full ${error.startdate ? 'border-red-500' : 'border-gray-300'} p-2 border rounded-lg`}
+            value={startdate}
+            placeholder='Start Date'
+            onChange={(e) => setstartdate(e.target.value)}
+          />
+          {error.startdate && <p className='text-red-500 mt-2 text-sm'>{error.startdate}</p>}
+        </div>
+        <div className='flex flex-col w-full'>
+          <input
+            type='date'
+            className={`w-full sm:w-full md:w-full ${error.enddate ? 'border-red-500' : 'border-gray-300'} p-2 border rounded-lg`}
+            value={enddate}
+            placeholder='End Date'
+            onChange={(e) => setenddate(e.target.value)}
+          />
+          {error.enddate && <p className='text-red-500 mt-2 text-sm'>{error.enddate}</p>}
+        </div>
+      </div>
+
+      <div className='flex flex-col w-full'>
+        <input
+          type='text'
+          className={`w-full p-2 ${error.owner ? 'border-red-500' : 'border-gray-300'} border rounded-lg`}
+          value={owner}
+          placeholder='Enter the Event Holder Name'
+          onChange={(e) => setowner(e.target.value)}
+        />
+        {error.owner && <p className='text-red-500 mt-2 text-sm'>{error.owner}</p>}
+      </div>
+
+      <div className='flex flex-col w-full'>
+        <input
+          type='text'
+          className={`w-full p-2 ${error.phone ? 'border-red-500' : 'border-gray-300'} border rounded-lg`}
+          value={phone}
+          placeholder='Enter the Phone Number'
+          onChange={(e) => setphone(e.target.value)}
+        />
+        {error.phone && <p className='text-red-500 mt-2 text-sm'>{error.phone}</p>}
+      </div>
+
+      <div className='flex flex-col w-full'>
+        <input
+          type='text'
+          className={`w-full p-2 ${error.address ? 'border-red-500' : 'border-gray-300'} border rounded-lg`}
+          value={address}
+          placeholder='Enter the Address'
+          onChange={(e) => setaddress(e.target.value)}
+        />
+        {error.address && <p className='text-red-500 mt-2 text-sm'>{error.address}</p>}
+      </div>
+      
+      <div className='flex flex-col w-full'>
+        <input
+          type='text'
+          className={`w-full p-2 ${error.city ? 'border-red-500' : 'border-gray-300'} border rounded-lg`}
+          value={city}
+          placeholder='Enter the City'
+          onChange={(e) => setcity(e.target.value)}
+        />
+        {error.city && <p className='text-red-500 mt-2 text-sm'>{error.city}</p>}
+      </div>
     </div>
   </div>
 
@@ -142,6 +205,7 @@ function Createevents() {
     </button>
   </div>
 </div>
+
   )
 }
 
