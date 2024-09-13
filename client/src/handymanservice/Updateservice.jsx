1ff0adddc84704df84e0de699aaf119665a005bf
alok7139 <alokgarg7139@gmail.com>
2024-09-13 22:15:13 +0530

    commit

--- client/src/handymanservice/Updateservice.jsx
+++ client/src/handymanservice/Updateservice.jsx
@@ -16,7 +16,43 @@ function Updateservice() {
   const [phone, setphone] = useState('')
   const [typeofservice, settypeofservice] = useState('')
 
-  
+  useEffect(() => {
+    const fetchdetails = async() => {
+      try {
+        await axios.get(`http://localhost:3000/api/v1/get/handyman/details/${id}` , {withCredentials:true})
+        .then((res) => {
+          const fetchdetails = res.data.fetchhandyman;
+          console.log(fetchdetails);
+          setname(fetchdetails.name)
+          setaddress(fetchdetails.address)
+          setfee(fetchdetails.fee)
+          setphone(fetchdetails.phone)
+          settypeofservice(fetchdetails.typeofservice);
+        })
+      } catch (error) {
+        toast.error(error.response.data.message)
+      }
+    }
+    fetchdetails();
+  })
+
+  const updateservice = async() => {
+      const formdata = new FormData();
+      formdata.append("name" , name)
+      formdata.append("fee" , fee)
+      formdata.append("phone" , phone)
+      formdata.append("typeofservice" , typeofservice)
+      formdata.append("address" , address);
+      try {
+        await axios.put(`http://localhost:3000/api/v1/update/service/${id}` , formdata , {withCredentials:true , headers:{"Content-Type" : "application/json"}}  )
+        .then((res) => {
+          toast.success(res.data.message);
+          navigate('/your/handyman/service')
+        })
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+  }
 
   return (
     <div>
