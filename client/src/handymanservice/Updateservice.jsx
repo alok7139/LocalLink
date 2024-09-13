09f27b0331eb2782ce24b99e86201483795e53f1
alok7139 <alokgarg7139@gmail.com>
2024-09-13 22:04:15 +0530

    commit

--- client/src/handymanservice/Updateservice.jsx
+++ client/src/handymanservice/Updateservice.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from '../main';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 function Updateservice() {
+
+  const { isauthenticated } = useContext(Context);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [name, setname] = useState('')
+  const [address, setaddress] = useState('')
+  const [fee, setfee] = useState('')
+  const [phone, setphone] = useState('')
+  const [typeofservice, settypeofservice] = useState('')
+
+  
+
   return (
     <div>
       Updateservice
--- client/src/handymanservice/Yourservice.jsx
+++ client/src/handymanservice/Yourservice.jsx
@@ -29,9 +29,10 @@ function YourHandymanservice() {
          try {
            await axios.delete(`http://localhost:3000/api/v1/delete/handyman/${id}` , {withCredentials:true})
            .then((res) => {
-            sethandymanservice((prevdata) => {
-              
-            })
+            sethandymanservice((prevdata) => ({
+              ...prevdata ,
+              handymanservice: prevdata.handymanservice.filter((handymanservice) => id !== handymanservice._id)
+            }))
             toast.success(res.data.message);
            }) 
          } catch (error) {
@@ -45,7 +46,42 @@ function YourHandymanservice() {
 
   return (
     <>
-      
+       <div className='mt-48 mb-20 font-serif'>
+    <div className='flex justify-center flex-col items-center mb-10 gap-5'>
+      <h1 className='text-4xl font-bold font-serif text-center'>Your Service</h1>
+    </div>
+
+    <div className='flex lg:p-5 p-4 flex-wrap justify-center items-center gap-5'>
+      {handymanservice && handymanservice.handymanservice &&  handymanservice.handymanservice.length>0 ? handymanservice.handymanservice.map((item) => (
+        <div 
+          key={item._id} 
+          className='bg-white rounded-xl border shadow-md p-3 w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.33%-1rem)]  hover:shadow-lg transition-all duration-500'>
+          <div className='p-3'>
+            <p className='font-bold text-lg'><span className='font-thin'>Handyman Provider Name : </span> {item.name}</p>
+            <p className='font-bold text-lg'><span className='font-thin'>Handyman Type : </span> {item.typeofservice}</p>
+            <p className='font-bold text-lg'><span className='font-thin'>Address : </span> {item.address}</p>
+            <p className='font-bold text-lg'><span className='font-thin'>Service Fee : </span>{item.fee}</p>
+            <p className='font-bold text-lg'><span className='font-thin'>Phone Number : </span>{item.phone}</p>
+          </div>
+          <div className='flex flex-col lg:flex-row gap-2 justify-between items-center mt-1'>
+  <Link to={`/Update/handyman/${item._id}`} className='w-full lg:w-auto'>
+    <button className='bg-blue-500 text-white py-2 px-4 rounded-lg w-full sm:w-auto'>Update</button>
+  </Link>
+ 
+  <button 
+    className='bg-red-500 text-white py-2 px-4 rounded-lg w-full sm:w-auto' 
+    onClick={() => deleteservice(item._id)}>
+    Delete
+  </button>
+
+</div>
+
+        </div>
+      ))  :
+      <h1>You didn't post any event yet</h1> 
+      }
+    </div>
+  </div>
     </>
   )
 }
