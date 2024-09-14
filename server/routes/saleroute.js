f72c285bcfc1d2c6b38e2d89b795c3266fa0c1aa
alok7139 <alokgarg7139@gmail.com>
2024-09-14 21:54:30 +0530

    commit

--- server/routes/saleroute.js
+++ server/routes/saleroute.js
@@ -1,4 +1,4 @@
-import { allsales, deletesale, registersale, usersale } from "../Controller/salecontroller.js";
+import { allsales, deletesale, registersale, updatesaleservice, usersale } from "../Controller/salecontroller.js";
 import express from 'express'
 import { isauthorized } from "../middlewares/auth.js";
 
@@ -9,5 +9,6 @@ router.post("/post/sale/service" , isauthorized , registersale);
 router.get("/get/allsale/post" ,allsales)
 router.get("/get/user/sale" , isauthorized , usersale);
 router.delete("/delete/sale/service/:id" , isauthorized , deletesale);
+router.put("/updated/sale/:id" , isauthorized , updatesaleservice)
 
 export default router;
