50588b58bde2cfb202282b214828f16b808cdc7d
alok7139 <alokgarg7139@gmail.com>
2024-09-16 21:41:05 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -19,31 +19,31 @@ cloudinary.v2.config({
 })
 
 
-// if(cluster.isPrimary){
-//     console.log(`Primary ${process.pid} is running`);
-//     for(let i=0;i<numCPUs;i++){
-//         cluster.fork();
-//     }
-// }
-// else{
+if(cluster.isPrimary){
+    console.log(`Primary ${process.pid} is running`);
+    for(let i=0;i<numCPUs;i++){
+        cluster.fork();
+    }
+}
+else{
     
-//     app.get("/" , (req,res) => {
-//         return res.json({
-//             message: `server is running on process id ${process.pid}`
+    app.get("/" , (req,res) => {
+        return res.json({
+            message: `server is running on process id ${process.pid}`
             
-//         })
-//         // console.log(process.pid);
-//     })
+        })
+        // console.log(process.pid);
+    })
 
-//     app.listen((process.env.PORT) , () => {
-//         console.log(`server is running on ${process.env.PORT}`)
-//     })
-// console.log(`Worker ${process.pid} started`);
-// }
+    app.listen((process.env.PORT) , () => {
+        console.log(`server is running on ${process.env.PORT}`)
+    })
+console.log(`Worker ${process.pid} started`);
+}
 
-app.listen((process.env.PORT) , () => {
-    console.log(`server is running on ${process.env.PORT}`)
-})
+// app.listen((process.env.PORT) , () => {
+//     console.log(`server is running on ${process.env.PORT}`)
+// })
 
 
 
